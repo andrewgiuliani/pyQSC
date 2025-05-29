e6bbb3612181e9ccdc7cad85ebd5cc7720689b64
Andrew Giuliani <agiuliani@flatironinstitute.org>
2025-05-29 10:48:20 -0400

    added unit test for B(phi), B'(phi), B''(phi) in the axisymmetric case

--- qsc/tests/test_grad_B_tensor.py
+++ qsc/tests/test_grad_B_tensor.py
@@ -121,6 +121,96 @@ class GradGradBTensorTests(unittest.TestCase):
                         np.testing.assert_allclose(s.grad_grad_B_alt[:,0,2,0], np.full(nphi, val), rtol=rtol, atol=atol)
                         np.testing.assert_allclose(s.grad_grad_B_alt[:,0,0,2], np.full(nphi, val), rtol=rtol, atol=atol)
                         
+class DirectionalDerivativeTests(unittest.TestCase):
+    def test_axisymmetry(self):
+        """
+        Test that the grad grad B tensor is correct for an axisymmetric vacuum field
+        """
+        B0_vals = [0.9, 1.1]
+        R0_vals = [0.85, 1.0, 1.2]
+        nphi_vals = [3, 5, 7, 9, 11]
+        etabar_vals = [0.55, 0.65, 0.95, 1.0, 1.09, 1.3, 1.4]
+        index = 0
+        for sG in [-1, 1]:
+            for spsi in [-1, 1]:
+                for nfp in range(1, 4):
+                    for j in range(3):
+                        """
+                        R0 = np.random.rand() * 2 + 0.3
+                        B0 = np.random.rand() * 2 + 0.3
+                        nphi = int(np.random.rand() * 10 + 3)
+                        etabar = (np.random.rand() - 0.5) * 4
+                        """
+                        # Try many combinations of values, in a deterministic way:
+                        R0 = R0_vals[np.mod(index, len(R0_vals))]
+                        B0 = B0_vals[np.mod(index, len(B0_vals))]
+                        nphi = nphi_vals[np.mod(index, len(nphi_vals))]
+                        etabar = etabar_vals[np.mod(index, len(etabar_vals))]
+                        index += 1
+                        
+                        s = Qsc(rc=[R0], zs=[0], nfp=nfp, nphi=nphi, sG=sG, \
+                                   spsi=spsi, B0=B0, etabar=etabar, I2=1.e-8, order='r2')
+                        # The O(r^2) solve fails if iota is exactly 0,
+                        # so above we set a tiny nonzero current to
+                        # create a tiny nonzero iota.
+                        if np.mod(nphi, 2) == 0:
+                            nphi += 1
+
+                        s.calculate_grad_grad_B_tensor(two_ways=True)
+                        # Check all components other than {tnn, ntn, nnt, ttt}. These should be 0.
+                        rtol = 1e-6
+                        atol = 1e-6
+                        
+                        # this test checks that the derivatives B'(\phi) and B''(\phi) 
+                        # when calculated by from the gradB and gradgradB tensors are correct
+                        axis_B0 = s.Bfield_cartesian(r=0, theta=0.)
+                        axis_B1 = s.grad_B_tensor_cartesian()
+                        axis_B2 = s.grad_grad_B_tensor_cartesian()
+                        
+                        from simsopt.geo import CurveRZFourier
+                        nfp=s.nfp
+                        G0 = CurveRZFourier(np.linspace(0, 1/nfp, s.phi.size, endpoint=False), s.rc.size-1, nfp, True)
+                        G0.rc[0] = R0
+                        G0.x = G0.get_dofs()
+
+                        dG0_dphi = G0.gammadash()
+                        d2G0_dphi2 = G0.gammadashdash()
+                        
+                        ds_dphi = np.linalg.norm(dG0_dphi, axis=1)
+                        d2s_dphi2 = np.sum(dG0_dphi*d2G0_dphi2)/ds_dphi[:, None]
+                        
+                        T, N, B = G0.frenet_frame()
+                        kappa = G0.kappa()
+                        dkappa_dphi = G0.kappadash()
+                        dkappa_ds = dkappa_dphi * (1/ds_dphi)
+                        torsion = G0.torsion()
+                        
+                        dT_ds = kappa[:, None] * N
+                        dN_ds = -kappa[:, None] * T + torsion[:, None]*B
+                        d2T_ds2 = dkappa_ds[:, None] * N + dN_ds*kappa[:, None]
+                        
+                        dT_dphi = dT_ds * ds_dphi[:, None]
+                        d2T_dphi2 = d2T_ds2*ds_dphi[:, None]**2 + d2s_dphi2*dT_ds
+                        
+                        Bexact = np.transpose(axis_B0, axes=(1, 0))
+                        Bqsc = sG*B0*T
+                        
+                        #check B(\phi)
+                        np.testing.assert_allclose(Bexact, Bqsc, rtol=rtol, atol=atol)
+                        
+                        dG0_dphi = np.transpose(dG0_dphi, axes=(1, 0))
+                        dBqsc_dphi = np.einsum('ijk,ik->jk', axis_B1, dG0_dphi)
+                        dBexact_dphi = sG*B0*dT_dphi
+                        dBexact_dphi = np.transpose(dBexact_dphi, axes=(1, 0))
+                        #check B'(\phi)
+                        np.testing.assert_allclose(dBexact_dphi, dBqsc_dphi, rtol=rtol, atol=atol)
+                        
+                        d2G0_dphi2 = np.transpose(d2G0_dphi2, axes=(1, 0))
+                        d2Bqsc_dphi2 = np.einsum('ijkl,jl,kl->il', axis_B2, dG0_dphi, dG0_dphi)+np.einsum('ijl,jl->il', axis_B1, d2G0_dphi2)
+                        d2Bexact_dphi2 = sG*B0*d2T_dphi2
+                        d2Bexact_dphi2 = np.transpose(d2Bexact_dphi2, axes=(1, 0))
+                        #check B''(\phi)
+                        np.testing.assert_allclose(d2Bexact_dphi2, d2Bqsc_dphi2, rtol=rtol, atol=atol)
 
 class CylindricalCartesianTensorsTests(unittest.TestCase):
 
